0d483d42956b5760e0160f53a1d7f29ef944966a
Luca Sanchez <lucasanchez1996@gmail.com>
2020-06-18 01:03:20 -0300

    Controlador de labotorio

---
+++ controller/LaboratorioController.java
@@ -0,0 +1,23 @@
+package controller;
+
+import java.util.ArrayList;
+
+import model.Paciente;
+import model.Peticion;
+import model.Practica;
+import model.Resultado;
+import model.Rol;
+import model.Sucursal;
+import model.Usuario;
+
+public class LaboratorioController {
+	
+	ArrayList<Usuario> usuarios;
+	ArrayList<Paciente> pacientes;
+	ArrayList<Sucursal> sucursales;
+	ArrayList<Practica> practicas;
+	ArrayList<Peticion> peticiones;
+	ArrayList<Resultado> resultados;
+	ArrayList<Rol> roles;
+	
+}
