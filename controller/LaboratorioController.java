aa1d22c40f39ca525a39c0dd15de378f16b84e05
Luca Sanchez <lucasanchez1996@gmail.com>
2020-06-18 02:31:19 -0300

    ABMs de pacientes, sucursales y practicas

--- controller/LaboratorioController.java
+++ controller/LaboratorioController.java
@@ -51,7 +51,7 @@ public class LaboratorioController {
 				return true;
 			}
 		}
-		System.out.println("No se encontre el usuario: " + usr);
+		System.out.println("No se encontro el usuario: " + usr);
 		return false;
 	}
 
@@ -76,5 +76,174 @@ public class LaboratorioController {
 	}
 	
 	
+	
+	public Paciente buscarPaciente (String dni) {
+		for (Paciente p : pacientes) {
+			if (p.getDNI().equals(dni)) {
+				return p;
+			}
+		}
+		return null;
+	}
+
+	public void altaPaciente(String DNI, String nombre, String domicilio, String mail, String sexo, String edad){
+		
+		if(buscarPaciente(DNI) == null){
+			
+			Paciente p = new Paciente(nombre, mail, domicilio, DNI, edad, sexo); //si no existe, creo nuevo paciente
+			pacientes.add(p);
+			System.out.println("El paciente " + nombre + " ha sido creado");
+		} else { //si existe, salgo sin crearlo
+			System.out.println("El paciente " + nombre + " ya existe en el sistema");
+		}
+	}
+
+	public boolean bajaPaciente(String dni) {
+		// busco al paciente en la lista de pacientes y lo borro si lo encuentro.
+
+		for (Paciente p : pacientes) {
+			if (p.getDNI().equals(dni)) {
+				pacientes.remove(p);
+				System.out.println("El paciente " + dni + " ha sido borrado");
+				return true;
+			}
+		}
+		System.out.println("No se encontro el paciente: " + dni);
+		return false;
+	}
+
+	public void modifPaciente(String nom, String dom, String mail, String dni, String edad, String sexo) {
+		for (Paciente p : pacientes) {
+			if (p.getDNI().equals(dni)) {
+				p.setNombre(nom);
+				p.setDomicilio(dom);
+				p.setMail(mail);
+				p.setSexo(sexo);
+				p.setEdad(edad);
+				System.out.println("El paciente " + dni + " ha sido modificado exitosamente");
+				System.out.println("-------");
+				System.out.println("Nombre: " + p.getNombre());
+				System.out.println("Domicilio: " + p.getDomicilio());
+				System.out.println("Mail: " + p.getMail());
+				System.out.println("Sexo: " + p.getSexo());
+				System.out.println("Edad: " + p.getSexo());
+				System.out.println("-------");
+				return;
+			}
+		}
+		System.out.println("El usuario " + dni + " no ha sido encontrado");
+	}
+	
+	
+	
+	public Sucursal buscarSucursal(String nro) {
+		for (Sucursal s : sucursales) {
+			if (s.getNumero().equals(nro)) {
+				return s;
+			}
+		}
+		return null;
+	}
+
+	public void altaSucursal(String numero, String direccion,String responsable_tecnico){
+		
+		if(buscarSucursal(numero) == null){
+			
+			Sucursal s = new Sucursal(numero, direccion, responsable_tecnico); //si no existe, creo nueva sucursal
+			sucursales.add(s);
+			System.out.println("La sucursal " + numero + " ha sido creada");
+		} else { //si existe, salgo sin crearlo
+			System.out.println("La sucursal " + numero + " ya existe en el sistema");
+		}
+	}
+
+	public boolean bajaSucursal(String nro) {
+		// busco la sucursal en la lista de sucursales y la borro si la encuentro
+
+		for (Sucursal s : sucursales) {
+			if (s.getNumero().equals(nro)) {
+				sucursales.remove(s);
+				System.out.println("La sucursal " + nro + " ha sido borrada");
+				return true;
+			}
+		}
+		System.out.println("No se encontro la sucursal: " + nro);
+		return false;
+	}
+
+	public void modifSucursal(String nro, String dir, String respTec) {
+		for (Sucursal s : sucursales) {
+			if (s.getNumero().equals(nro)) {
+				s.setDireccion(dir);
+				s.setResponsable_tecnico(respTec);
+				System.out.println("La sucursal " + nro + " ha sido modificada exitosamente");
+				System.out.println("-------");
+				System.out.println("Direccion: " + s.getDireccion());
+				System.out.println("Domicilio: " + s.getResponsable_tecnico());
+				System.out.println("-------");
+				return;
+			}
+		}
+		System.out.println("La sucursal " + nro + " no ha sido encontrada");
+	}
+	
+	
+	
+	public Practica buscarPractica(String cod) {
+		for (Practica c : practicas) {
+			if (c.getCodigo_practica().equals(cod)) {
+				return c;
+			}
+		}
+		return null;
+	}
+
+	public void altaPractica(String codigo_practica, String nombre_practica, String grupo, String valores_criticos, String valores_reservados, String demora_resultado){
+		
+		if(buscarPractica(codigo_practica) == null){
+			
+			Practica c = new Practica(codigo_practica, nombre_practica, grupo, valores_criticos, valores_reservados, demora_resultado); //si no existe, creo nueva practica
+			practicas.add(c);
+			System.out.println("La practica " + codigo_practica + " ha sido creado");
+		} else { //si existe, salgo sin crearlo
+			System.out.println("La practica " + codigo_practica + " ya existe en el sistema");
+		}
+	}
+
+	public boolean bajaPractica(String cod) {
+		// busco la practica en la lista de practicas y la borro si la encuentro
+
+		for (Practica c : practicas) {
+			if (c.getCodigo_practica().equals(cod)) {
+				practicas.remove(c);
+				System.out.println("La practica " + cod + " ha sido borrada");
+				return true;
+			}
+		}
+		System.out.println("No se encontro la practica: " + cod);
+		return false;
+	}
+
+	public void modifUsuario(String cod, String nom, String grupo, String valCriticos, String valReservados, String demora) {
+		for (Practica c : practicas) {
+			if (c.getCodigo_practica().equals(cod)) {
+				c.setNombre_practica(nom);
+				c.setGrupo(grupo);
+				c.setValores_criticos(valCriticos);
+				c.setValores_reservados(valReservados);
+				c.setDemora_resultado(demora);
+				System.out.println("La practica " + cod + " ha sido modificada exitosamente");
+				System.out.println("-------");
+				System.out.println("Nombre: " + c.getNombre_practica());
+				System.out.println("Grupo: " + c.getGrupo());
+				System.out.println("Valores Criticos: " + c.getValores_criticos());
+				System.out.println("Valores Reservados: " + c.getValores_reservados());
+				System.out.println("Demora de los resultados: " + c.getDemora_resultado());
+				System.out.println("-------");
+				return;
+			}
+		}
+		System.out.println("La practica " + cod + " no ha sido encontrada");
+	}
 	
 }
